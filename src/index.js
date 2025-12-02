16637e6d127554dd90e252b1253ce3457efc5b72
Hellen <hellensaandrade@gmail.com>
2025-12-02 20:08:39 -0300

    data2008

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
-import express from "express"
-import {people} from "./people.js"
+import express, { response } from "express"
+import cors from "cors"
+import mysql from "mysql2"
+import { people } from "./people.js"
 
 const app = express()
 const port = 3333
 
-app.get("/",(request,response) =>{
+app.use(cors())
+app.use(express.json())
+
+app.post("/Cadastrar",(request,response) =>{
+   const {name,email,age,password} = request.body.user
+
+
+const incerCommand = `
+INSERT INTO
+`
+
+   console.log(
+    `
+    Nome: ${name},
+    Email: ${email},
+    Idade: ${age},
+    Senha : ${password}
+    `
+   )
+
+
+   response.status(201).json({message: "Usuário cadatrado com sucesso!"})
+})
+
+app.get("/", (request, response) => {
     response.json(people)
 })
 
 app.listen(port, () => {
-    console.log('Servidor rodando na porta: ${port}')
+    console.log(`Servidor rodando na porta: ${port}`)
+})
 
+mysql.createPool({
+    database: "web_02mc",
+    host: "benserverplex.ddns.net",
+    user:"alunos",
+    password:"senhaAlunos",
+    connectionLimit:10
 })
